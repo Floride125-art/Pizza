ef8bcff8776fe3f331ba37d366b47d7cc1b73da9
Floride125-art <FTuyisenge120@daviscollege.com>
2020-10-10 19:18:18 +0200

    added javascript codes to my delivery form

--- js/index.js
+++ js/index.js
@@ -44,3 +44,14 @@ $(document).ready(function(){
     $("#formD").show();
   });
 });
+$(document).ready(function(){
+  $("form#formD").submit(function(event){
+      var Name = $("input#Name").val();
+      var Phone = $("input#Phone").val();
+      var Location = $("input#Location").val();
+      alert("Dear " + Name + " Who is located at " + Location + " we will get back to you on " + Phone + " Whenever the Pizza are delivered to you");
+      event.preventDefault();
+
+  });
+
+});
